5a042181691eab040e93df19d68deb7b8c12de1b
binhkid2 <binhkid2@gmail.com>
2024-06-05 16:08:18 +0700

    aaaaa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { useAtom } from 'jotai';
-import  { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { zaloAccessTokenAtom } from '../store';
 import { generate_pkce_codes, generate_state_param } from '../Utils/crypto';
 
@@ -7,11 +7,10 @@ export default function HomePage() {
     const [zaloAccessToken, setZaloAccessToken] = useAtom(zaloAccessTokenAtom);
     const [isLogged, setIsLogged] = useState(false);
     const [userName, setUserName] = useState('');
-    const [userAvatar,setUserAvatar]= useState('');
+    const [userAvatar, setUserAvatar] = useState('');
     const [userId, setUserId] = useState('');
 
     useEffect(() => {
-        console.log(import.meta.env.VITE_ZALO_PERMISSION_URL)
         const fetchData = async () => {
             try {
                 if (!zaloAccessToken) {
@@ -26,10 +25,9 @@ export default function HomePage() {
                     })}`;
                     window.location.replace(auth_uri);
                 } else {
-                  setIsLogged(true)
+                    setIsLogged(true);
                     // Store the INITIAL access token in a JavaScript constant.
                     const initialToken = zaloAccessToken;
-                    alert(initialToken);
                     const user_access_token = JSON.parse(initialToken).access_token;
 
                     // Display information of user
@@ -48,7 +46,7 @@ export default function HomePage() {
                     if (data.message.toLowerCase() === "success") {
                         setUserName(data.name);
                         setUserId(data.id);
-                        setUserAvatar(data.picture.data.url)
+                        setUserAvatar(data.picture.data.url);
                     } else {
                         setUserName("Unknown");
                         setUserId("Unknown");
@@ -63,7 +61,7 @@ export default function HomePage() {
         };
 
         fetchData();
-    }, [zaloAccessToken]);
+    }, [zaloAccessToken, setZaloAccessToken]);
 
     if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
         return <h1>Please use a domain.</h1>;
@@ -81,18 +79,20 @@ export default function HomePage() {
                     </div>
                 ) : (
                     <table style={{ border: "none", borderTop: "1px solid black" }}>
-                        <tr>
-                            <td>id:</td>
-                            <td>{userId}</td>
-                        </tr>
-                        <tr>
-                            <td>name:</td>
-                            <td>{userName}</td>
-                        </tr>
-                        <tr>
-                            <td>picture:</td>
-                            <td><img src={userAvatar} alt="avatar" width="120" height="120" /></td>
-                        </tr>
+                        <tbody>
+                            <tr>
+                                <td>id:</td>
+                                <td>{userId}</td>
+                            </tr>
+                            <tr>
+                                <td>name:</td>
+                                <td>{userName}</td>
+                            </tr>
+                            <tr>
+                                <td>picture:</td>
+                                <td><img src={userAvatar} alt="avatar" width="120" height="120" /></td>
+                            </tr>
+                        </tbody>
                     </table>
                 )}
             </>
