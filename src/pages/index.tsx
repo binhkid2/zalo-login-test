2578be45bda70a2f8a7fa07dbcb0b6645da9a841
binhkid2 <binhkid2@gmail.com>
2024-06-05 16:13:40 +0700

    auth_uri

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { useAtom } from 'jotai';
-import { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import { zaloAccessTokenAtom } from '../store';
 import { generate_pkce_codes, generate_state_param } from '../Utils/crypto';
 
@@ -7,7 +7,7 @@ export default function HomePage() {
     const [zaloAccessToken, setZaloAccessToken] = useAtom(zaloAccessTokenAtom);
     const [isLogged, setIsLogged] = useState(false);
     const [userName, setUserName] = useState('');
-    const [userAvatar, setUserAvatar] = useState('');
+    const [userAvatar,setUserAvatar]= useState('');
     const [userId, setUserId] = useState('');
 
     useEffect(() => {
@@ -17,17 +17,19 @@ export default function HomePage() {
                     const state = generate_state_param(); // for CSRF prevention
                     // Generate the code verifier and code challenge
                     const codes = generate_pkce_codes();
-                    const auth_uri = `${import.meta.env.VITE_ZALO_PERMISSION_URL}?${new URLSearchParams({
-                        app_id: import.meta.env.VITE_APP_ID,
-                        redirect_uri: import.meta.env.VITE_APP_REDIRECT_URI,
+                    const auth_uri = `${import.meta.env.ZALO_PERMISSION_URL}?${new URLSearchParams({
+                        app_id: import.meta.env.APP_ID,
+                        redirect_uri: import.meta.env.APP_REDIRECT_URI,
                         code_challenge: codes.challenge,
                         state: state, // <- prevent CSRF
                     })}`;
+                    console.log("auth_uri: ",auth_uri)
                     window.location.replace(auth_uri);
                 } else {
-                    setIsLogged(true);
+                  setIsLogged(true)
                     // Store the INITIAL access token in a JavaScript constant.
                     const initialToken = zaloAccessToken;
+                    alert(initialToken);
                     const user_access_token = JSON.parse(initialToken).access_token;
 
                     // Display information of user
@@ -46,7 +48,7 @@ export default function HomePage() {
                     if (data.message.toLowerCase() === "success") {
                         setUserName(data.name);
                         setUserId(data.id);
-                        setUserAvatar(data.picture.data.url);
+                        setUserAvatar(data.picture.data.url)
                     } else {
                         setUserName("Unknown");
                         setUserId("Unknown");
@@ -61,7 +63,7 @@ export default function HomePage() {
         };
 
         fetchData();
-    }, [zaloAccessToken, setZaloAccessToken]);
+    }, [zaloAccessToken]);
 
     if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
         return <h1>Please use a domain.</h1>;
@@ -79,20 +81,18 @@ export default function HomePage() {
                     </div>
                 ) : (
                     <table style={{ border: "none", borderTop: "1px solid black" }}>
-                        <tbody>
-                            <tr>
-                                <td>id:</td>
-                                <td>{userId}</td>
-                            </tr>
-                            <tr>
-                                <td>name:</td>
-                                <td>{userName}</td>
-                            </tr>
-                            <tr>
-                                <td>picture:</td>
-                                <td><img src={userAvatar} alt="avatar" width="120" height="120" /></td>
-                            </tr>
-                        </tbody>
+                        <tr>
+                            <td>id:</td>
+                            <td>{userId}</td>
+                        </tr>
+                        <tr>
+                            <td>name:</td>
+                            <td>{userName}</td>
+                        </tr>
+                        <tr>
+                            <td>picture:</td>
+                            <td><img src={userAvatar} alt="avatar" width="120" height="120" /></td>
+                        </tr>
                     </table>
                 )}
             </>
