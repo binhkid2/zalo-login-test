c86c42ee96803639bfb0c722a355f9c156beb2a3
LacChinhSoft <128137927+binhkid2@users.noreply.github.com>
2024-06-06 04:15:11 +0000

    DashboardLayout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+export function DashboardLayout({ children }: DashboardLayoutProps) {
+  return (
+    
+    
+              <main className="">{children}</main>
+  )
+          
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import { DashboardLayout } from "./components/Layout"; 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+  <React.StrictMode> 
     <Router>
+      <DashboardLayout>
         <App />
-    </Router>
+      </DashboardLayout>
+    </Router> 
   </React.StrictMode>
 );
